6f56dd2151c164f5308fcbf4912c6af4eb558b48
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-03-27 23:57:38 +0300

    list: Add more list manipulation predicates

--- lib/list.pl
+++ lib/list.pl
@@ -1,17 +1,29 @@
 :- module(list).
 
 :- export generate_list/2.
+:- export generate_list/3.
 :- export remove/3.
 :- export insert/3.
 :- export sublist/2.
+:- export map_list_to_position/2.
+:- export run_length_decoding/2.
+:- export halve/3.
 
 % generate_list(+C, -L).
-% Given natural number C, L is a list of C zeros.
+% Given natural number C, L is a list of C count zeros.
 generate_list(0, []).
 generate_list(C, [0|L]) :-
     C > 0,
     C1 is C - 1,
     generate_list(C1, L).
+
+% generate_list(+C, +E, -L).
+% Given natural number C and element E, L is a list of C count Es.
+generate_list(0, _, []).
+generate_list(C, E, [E|L]) :-
+    C > 0,
+    C1 is C - 1,
+    generate_list(C1, E, L).
 
 % remove(+E, +L1, -L2).
 % Given element E and list L1, L2 is the result of remove E from anywhere in L1.
@@ -31,4 +43,39 @@ insert(E, L1, L2) :-
 sublist(L1, L2) :-
     append(L3, _, L2),
     append(_, L1, L3).
+
+% map_list_to_position(+L, -ML).
+% Given a list L maps every element of L to its position in the list by
+% creating a list of tuples of the form (element, position).
+map_list_to_position(L, ML) :-
+    map_list_to_position(L, 1, ML).
+map_list_to_position([], _, []).
+map_list_to_position([H|T], Pos, [(H, Pos)|MT]) :-
+    NextPos is Pos + 1,
+    map_list_to_position(T, NextPos, MT).
+
+% run_length_decoding(+Enc, -Dec).
+% Given an encoded run-length list of (run-length, element) tuples, produces
+% the decoded list.
+run_length_decoding([], []).
+run_length_decoding([(C, E)|T], L) :-
+    generate_list(C, E, L1),
+    run_length_decoding(T, L2),
+    append(L1, L2, L).
+
+% halve(+L, -L1, -L2).
+% Given list L, L1 and L2 are the two halfs of L.
+% If the length of L is 2*n+1, L1 is of length n and L2 is of length n + 1.
+halve(L, L1, L2) :-
+    halve(L, L, L1, L2).
+
+% halve(+L, +L, -L1, -L2).
+% The first and the second argument have to be the list to be halved.
+% Iteratively remove 2 elements from the first and 1 element from the second.
+% When the first argument list ends up being [] or [_], the second argument list
+% is the second half of the original list.
+halve([], L2, [], L2).
+halve([_], L2, [], L2).
+halve([_,_|T], [X|L], [X|L1], L2) :-
+    halve(T, L, L1, L2).
 
