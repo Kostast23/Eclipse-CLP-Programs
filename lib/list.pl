ef601c907698fca0e38e34eda91a5ebaa339ca1c
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-04-12 22:36:39 +0300

    list: Order predicates aplhabetically

--- lib/list.pl
+++ lib/list.pl
@@ -1,11 +1,11 @@
 :- module(list).
 
 :- export generate_list/3.
-:- export remove/3.
-:- export insert/3.
-:- export sublist/2.
-:- export map_list_to_position/2.
 :- export halve/3.
+:- export insert/3.
+:- export map_list_to_position/2.
+:- export remove/3.
+:- export sublist/2.
 
 
 % generate_list(+C, +E, -L).
@@ -16,34 +16,6 @@ generate_list(C, E, [E|L]) :-
     C1 is C - 1,
     generate_list(C1, E, L).
 
-% remove(+E, +L1, -L2).
-% Given element E and list L1, L2 is the result of remove E from anywhere in L1.
-remove(E, [E|L], L).
-remove(E, [X|L1], [X|L2]) :-
-    remove(E, L1, L2).
-
-% insert(+E, +L1, -L2).
-% Given element E and list L1, L2 is the result of inserting E anywhere in L1.
-insert(E, L1, L2) :-
-    remove(E, L2, L1).
-
-% sublist(+L1, +L2)
-% Checks if list L1 is a sublist of L2.
-% Avoid using this predicate to find all the possible sublists of L2 as the
-% empty list may appear multiple times in the result.
-sublist(L1, L2) :-
-    append(L3, _, L2),
-    append(_, L1, L3).
-
-% map_list_to_position(+L, -ML).
-% Given a list L maps every element of L to its position in the list by
-% creating a list of tuples of the form (element, position).
-map_list_to_position(L, ML) :-
-    map_list_to_position(L, 1, ML).
-map_list_to_position([], _, []).
-map_list_to_position([H|T], Pos, [(H, Pos)|MT]) :-
-    NextPos is Pos + 1,
-    map_list_to_position(T, NextPos, MT).
 
 % halve(+L, -L1, -L2).
 % Given list L, L1 and L2 are the two halfs of L.
@@ -60,4 +32,37 @@ halve([], L2, [], L2).
 halve([_], L2, [], L2).
 halve([_,_|T], [X|L], [X|L1], L2) :-
     halve(T, L, L1, L2).
+
+
+% insert(+E, +L1, -L2).
+% Given element E and list L1, L2 is the result of inserting E anywhere in L1.
+insert(E, L1, L2) :-
+    remove(E, L2, L1).
+
+
+% map_list_to_position(+L, -ML).
+% Given a list L maps every element of L to its position in the list by
+% creating a list of tuples of the form (element, position).
+map_list_to_position(L, ML) :-
+    map_list_to_position(L, 1, ML).
+map_list_to_position([], _, []).
+map_list_to_position([H|T], Pos, [(H, Pos)|MT]) :-
+    NextPos is Pos + 1,
+    map_list_to_position(T, NextPos, MT).
+
+
+% remove(+E, +L1, -L2).
+% Given element E and list L1, L2 is the result of remove E from anywhere in L1.
+remove(E, [E|L], L).
+remove(E, [X|L1], [X|L2]) :-
+    remove(E, L1, L2).
+
+
+% sublist(+L1, +L2)
+% Checks if list L1 is a sublist of L2.
+% Avoid using this predicate to find all the possible sublists of L2 as the
+% empty list may appear multiple times in the result.
+sublist(L1, L2) :-
+    append(L3, _, L2),
+    append(_, L1, L3).
 
