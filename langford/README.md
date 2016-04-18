9d3cc470a7dd51ad79ab91440d60af257f108e90
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-04-18 10:51:02 +0300

    langford: Add README

---
+++ langford/README.md
@@ -0,0 +1,7 @@
+A Langford sequence, is a permutation of the sequence of 2n numbers 1, 1, 2,
+2, ..., n, n in which the two ones are one unit apart, the two twos are two
+units apart, and more generally the two copies of each number k are k units
+apart. Langford pairings are named after C. Dudley Langford, who posed the
+problem of constructing them in 1958.
+
+Source: [Wikipedia](https://en.wikipedia.org/wiki/Langford_pairing)
--- langford/langford.pl
+++ langford/langford.pl
@@ -1,11 +1,5 @@
 :- use_module("../lib/list").
 
-% Wikipedia on Langford pairing/sequence.
-% A Langford sequence, is a permutation of the sequence of 2n numbers 1, 1, 2,
-% 2, ..., n, n in which the two ones are one unit apart, the two twos are two
-% units apart, and more generally the two copies of each number k are k units
-% apart. Langford pairings are named after C. Dudley Langford, who posed the
-% problem of constructing them in 1958.
 
 % langford(+N, -L).
 % Given number N > 1, L is the corresponding Langford sequence.
